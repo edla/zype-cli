69ebaac4af7f0e59de1480ef1c86a3aca7bb4c3d
Adi Singh <adityasingh89@gmail.com>
2014-04-06 23:15:13 -0400

    fixed rainbow requirement + progressbar for needed for commands

--- lib/zype/commands/video.rb
+++ lib/zype/commands/video.rb
@@ -1,5 +1,5 @@
 require "thor"
-require 'progressbar'
+require 'zype/progress_bar'
 require 'zype/file_reader'
 
 module Zype
@@ -8,31 +8,31 @@ module Zype
 
     method_option "filters", aliases: "f", type: :hash,    default: {}, desc: "Specify video filters"
     method_option "page",    aliases: "p", type: :numeric, default: 0,  desc: "Specify the page of videos to return"
-    method_option "count",   aliases: "c", type: :numeric, default: 10, desc: "Specify the number of videos to return"      
+    method_option "count",   aliases: "c", type: :numeric, default: 10, desc: "Specify the number of videos to return"
 
     define_method "video:list" do
       load_configuration
-      
+
       videos = Zype::Client.new.videos.all(options[:filters], options[:page], options[:count])
-      
+
       puts "Found #{videos.size} video(s)"
       puts "---"
-      
+
       videos.each do |video|
         print_video(video)
       end
     end
-    
+
     desc "video:update", "Updates a video"
-    
+
     method_option "id", aliases: "i", type: :string, required: true, desc: "The video to update"
-    
+
     method_option "title", aliases: "t", type: :string, desc: "New video title"
     method_option "keywords", aliases: "k", type: :array, desc: "New video keywords"
-    
+
     define_method "video:update" do
       load_configuration
-      
+
       if video = Zype::Client.new.videos.find(options[:id])
         video.title = options[:title] if options[:title]
         video.keywords = options[:keywords] if options[:keywords]
@@ -41,36 +41,36 @@ module Zype
       end
       puts ""
     end
-    
+
     desc "video:upload", "Uploads a video"
-    
+
     method_option "filename", aliases: "f", type: :string, required: false, desc: "File path to upload"
     method_option "directory", aliases: "d", type: :string, required: false, desc: "Directory to upload"
-    
+
     method_option "chunksize", aliases: "c", type: :numeric, default: 512, desc: "Chunk size (KB)"
-    
+
     define_method "video:upload" do
       load_configuration
-          
-      uploads = []      
+
+      uploads = []
 
       if filename = options[:filename]
         uploads << upload_video(filename)
-      end      
+      end
       if directory = options[:directory]
         Dir.foreach(directory) do |filename|
           next if filename == '.' or filename == '..'
           uploads << upload_video(directory + "/" + filename)
         end
       end
-      
+
       uploads.each do |u|
         transcode_video(u)
       end
     end
-  
+
     no_commands do
-    
+
       def print_video(video)
         puts "Title: #{video.title} (ID: #{video._id})"
         puts "Attributes:"
@@ -79,30 +79,30 @@ module Zype
         end
         puts "---"
       end
-      
+
       def transcode_video(upload)
         Zype::Client.new.videos.create(title: upload.filename, upload_id: upload["_id"])
       end
-      
-      def upload_video(filename)        
+
+      def upload_video(filename)
         file = File.open(filename)
         basename = File.basename(filename)
         size = options[:chunksize]  * 1024
-      
+
         upload = Zype::Client.new.uploads.create(filename: basename, filesize: file.size)
-      
+
         begin
           uri = URI.parse(upload["upload_url"])
-      
+
           pbar = Zype::ProgressBar.new(basename, file.size)
-       
+
           last_check_in = Time.now
-      
+
           chunked = Zype::FileReader.new(file, size) do |file_reader|
 
             pbar.set(file_reader.current)
             progress = (file_reader.current.to_f / file.size * 100).floor
-        
+
             if  last_check_in < Time.now - 5 # seconds
               upload.progress = progress
               upload.status = 'uploading'
@@ -110,21 +110,21 @@ module Zype
               last_check_in = Time.now
             end
           end
-      
+
           http = Net::HTTP.new(uri.host, uri.port)
           http.use_ssl = true
 
           req = Net::HTTP::Put.new(uri.request_uri)
-          req.body_stream  = chunked     
+          req.body_stream  = chunked
           req["Content-Type"] = "multipart/form-data"
           req.add_field('Content-Length', File.size(file))
-                  
+
           res = http.request(req)
 
           upload.progress = 100
           upload.status = 'complete'
-          
-          
+
+
         rescue Interrupt => e
           puts "Upload Cancelled"
           upload.status = 'cancelled'
@@ -139,7 +139,7 @@ module Zype
         upload
       end
 
-    end  
-  
+    end
+
   end
 end
--- lib/zype/progress_bar.rb
+++ lib/zype/progress_bar.rb
@@ -1,4 +1,4 @@
-require 'rainbow/ext/string'
+require 'rainbow'
 
 module Zype
   #
