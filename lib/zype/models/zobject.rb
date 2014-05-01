763521f509451afba5505a7bb986fe94441bb691
Adi Singh <adityasingh89@gmail.com>
2014-05-01 01:29:21 -0400

    videos helper on zobject model

--- lib/zype/models/zobject.rb
+++ lib/zype/models/zobject.rb
@@ -1,4 +1,14 @@
 module Zype
   class Zobject < Zype::Model
+
+    def videos
+      array = self.video_ids
+      videos = []
+      unless array.empty?
+        id_string = array.join(',')
+        videos.push(service.videos.find(id_string)).flatten
+      end
+    end
+
   end
 end
