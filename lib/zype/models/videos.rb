7be8007a64dde69b4940ecfe91fb8b4e52403bad
Adi Singh <adityasingh89@gmail.com>
2014-05-05 17:07:58 -0400

    added extra params for order

--- lib/zype/models/videos.rb
+++ lib/zype/models/videos.rb
@@ -2,8 +2,8 @@ module Zype
   class Videos < Zype::Collection
     model Video
 
-    def all(filters={}, page=0, per_page=10)
-      load(service.get('/videos', filters: filters, page: page, per_page: per_page))
+    def all(filters={}, page=0, per_page=10, sort="title", order="asc")
+      load(service.get('/videos', filters: filters, page: page, per_page: per_page, sort: sort, order: order))
     end
 
     def find(id)
