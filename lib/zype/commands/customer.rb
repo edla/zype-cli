45298259de140bbcecfd1fde0788ed2d64d8d449
Landon Marder <landon.marder@gmail.com>
2014-10-22 11:54:26 -0400

    add get of video favorites for specific customer

--- lib/zype/commands/customer.rb
+++ lib/zype/commands/customer.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'pry'
 
 module Zype
   class Commands < Thor
@@ -38,11 +39,27 @@ module Zype
       print_customers([customer])
     end
 
+    desc 'customer:favorite_videos', 'List Favorite Videos'
+
+    method_option 'customer_id', aliases: 'i', type: :string, required: true, desc: 'Customer ID'
+
+    define_method 'customer:favorite_videos' do
+      init_client
+
+      customer = @zype.customers.find(options[:customer_id])
+
+      print_video_favorites(customer['video_favorites'])
+    end
+
+
     no_commands do
 
       def print_customers(customers)
-        # binding.pry
         puts Hirb::Helpers::Table.render(customers, :fields=>[:_id, :email, :subscription_count, :card_count, :video_favorites, :created_at])
+      end
+
+      def print_video_favorites(video_favorites)
+        puts Hirb::Helpers::Table.render(video_favorites, :fields => [:_id, :video_id, :created_at, :updated_at])
       end
     end
   end
--- lib/zype/models/customer.rb
+++ lib/zype/models/customer.rb
@@ -1,5 +1,20 @@
+require 'pry'
 module Zype
   class Customer < Zype::Model
 
+    def video_favorites(options = {})
+      service.get("/customers/#{self['_id']}/video_favorites", options)["response"]
+    end
+
+    def favorite_video(video_id=nil)
+      binding.pry
+      service.post("/customers/#{self['_id']}/video_favorites", video_id: video_id)
+    end
+
+    def unfavorite_video(video_id=nil)
+      binding.pry
+      # Use the video_id and customer_id to get the video_favorite_id
+      service.delete("/customers/#{self['id']}/video_favorites/#{video_favorite_id}")
+    end
   end
 end
