e73d8790bceee8abd12a96ca04cc7df6351946d3
Brian Dorry <brian.dorry@gmail.com>
2014-10-22 15:01:47 -0400

    renamed customer to consumer in card models

--- lib/zype/models/cards.rb
+++ lib/zype/models/cards.rb
@@ -2,16 +2,16 @@ module Zype
   class Cards < Zype::Collection
     model Card
 
-    def all(customer_id,params={})
-      load(service.get("/customers/#{customer_id}/cards", params))
+    def all(consumer_id,params={})
+      load(service.get("/consumers/#{consumer_id}/cards", params))
     end
 
-    def find(customer_id,id)
-      load(service.get("/customers/#{customer_id}/cards#{id}"))
+    def find(consumer_id,id)
+      load(service.get("/consumers/#{consumer_id}/cards#{id}"))
     end
 
-    def create(customer_id,attributes={})
-      load(service.post("/customers/#{customer_id}/cards", card: attributes))
+    def create(consumer_id,attributes={})
+      load(service.post("/consumers/#{consumer_id}/cards", card: attributes))
     end
   end
 end
